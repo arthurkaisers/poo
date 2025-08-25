21a8b74800668d43f04bff89ad618e4f116155ea
ARTHUR KAISER DA SILVA <arthur07.silva@catolicasc.edu.br>
2025-08-25 11:40:11 -0300

    primeiro commit

---
+++ BibliotecaConsole/Livro.cs
@@ -0,0 +1,30 @@
+public class Livro
+{
+    //propriedades
+    public string isbn;
+    public string titulo;
+    public string autor;
+    public int paginas;
+
+
+    //métodos
+    public Livro(string cod, string tit, string aut, int pag)
+    {
+        isbn = cod;
+        titulo = tit;
+        autor = aut;
+        paginas = pag;
+    }
+
+    public void ImprimirLivro(int linha)
+    {
+        Console.SetCursorPosition(0, linha);
+        Console.Write(isbn);
+        Console.SetCursorPosition(19, linha);
+        Console.Write(titulo);
+        Console.SetCursorPosition(49, linha);
+        Console.Write(autor);
+        Console.SetCursorPosition(65, linha);
+        Console.Write(paginas);
+    }
+}
